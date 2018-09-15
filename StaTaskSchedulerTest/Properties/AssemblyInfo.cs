522e5cbd39f9c57c9c3a261a7f12f7b7c691ebc4
s-yk <sonic_cave@hotmail.co.jp>
2018-09-15 11:02:15 +0900

    Test・使用例の追加

---
+++ StaTaskSchedulerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StaTaskSchedulerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StaTaskSchedulerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bafb1086-27be-4064-aaf8-cdba04822996")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StaTaskSchedulerTest/StaTaskSchedulerTest.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.IO.Packaging;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Xps.Packaging;
+using NUnit.Framework;
+
+namespace StaTaskSchedulerTest
+{
+    [TestFixture]
+    public class StaTaskSchedulerTest
+    {
+        private const string TestDataPath = @"TestData\XpsSample.xps";
+
+        [Test]
+        public void StaTaskSchedulerの使用例()
+        {
+            var loadDocumentTask = Task.Factory.StartNew(LoadDocument,
+                CancellationToken.None,
+                TaskCreationOptions.None,
+                new StaTaskScheduler.StaTaskScheduler());
+
+            Assert.That(() => loadDocumentTask.Wait(), Throws.Nothing);
+            Assert.That(loadDocumentTask.IsCompleted, Is.True);
+            Assert.That(loadDocumentTask.Result, Is.Not.Null);
+            Assert.That(loadDocumentTask.Result.DocumentPaginator.PageCount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void DefaultTaskSchedulerの例()
+        {
+            var loadDocumentTask = Task.Factory.StartNew(LoadDocument,
+                CancellationToken.None,
+                TaskCreationOptions.None,
+                TaskScheduler.Default);
+
+            Assert.That(() => loadDocumentTask.Wait(), Throws.Exception);
+            Assert.That(loadDocumentTask.IsFaulted, Is.True);
+            Assert.That(loadDocumentTask.Exception.InnerException, Is.TypeOf<System.Windows.Markup.XamlParseException>());
+        }
+
+        /// <summary>
+        /// XPSの読み込み
+        /// 要STA実行
+        /// </summary>
+        /// <returns></returns>
+        private IDocumentPaginatorSource LoadDocument()
+        {
+            var path = Path.Combine(new DirectoryInfo(Assembly.GetExecutingAssembly().Location).Parent.FullName, TestDataPath);
+            IDocumentPaginatorSource source;
+            using (var xpsDocument = new XpsDocument(path, FileAccess.Read, CompressionOption.NotCompressed))
+            {
+                source = xpsDocument.GetFixedDocumentSequence() as IDocumentPaginatorSource;
+            }
+
+            return source;
+        }
+    }
+}
